b586477f2eedeca0ea36c17eb5bc858b3fd97f2c
Genedy <genedymohamed96@gmail.com>
2017-01-15 02:27:06 +0200

    adjusting users table and creating music table

--- database/migrations/2017_01_14_223416_CreateMusicTable.php
+++ database/migrations/2017_01_14_223416_CreateMusicTable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMusicTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('music', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
-            $table->string('password');
-            $table->string('likes');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('reviews_no');
+            $table->string('link');
             $table->timestamps();
-        });
-    }
+        });    }
 
     /**
      * Reverse the migrations.
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('music');
     }
 }
